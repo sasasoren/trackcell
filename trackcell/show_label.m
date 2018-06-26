edea75fabe5be0e179d1953a183b50cecfb7eaa7
Sorena Sarmadi <sorena@math.uh.edu>
2018-06-26 13:16:49 -0500

    first test commit

--- trackcell/show_label.m
+++
@@ -1,9 +0,0 @@
-function show = show_label(L_perfect)
-%this function shows label of each cell
-lpic = bwlabel(L_perfect);
-for i = 1:max(max(lpic))
-    [I J]=ind2sub(size(lpic),find(lpic ==i));
-    Cx(i)= floor(sum(I)/length(I));
-    Cy(i)= floor(sum(J)/length(J));
-    text(Cy(i)-4,Cx(i),num2str(i),'Color','red');
-end
